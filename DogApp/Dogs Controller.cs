c1aa305f7e023ed471a8315a39ce6d9b45a6b0e9
marti7734 <m.ivanov2018@pmg-pernik.com>
2023-02-06 13:20:45 +0200

    1

---
+++ DogApp/Dogs Controller.cs
@@ -0,0 +1,29 @@
+﻿using DogApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogApp
+{
+    public class Dogs_Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private readonly ApplicationDbContext context;
+
+        public Dogs_Controller(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
